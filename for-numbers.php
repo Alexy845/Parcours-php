edcdbf425a2b8dcf4da08403cbcfa3e9214177b2
Alexy845 <alexy.daubresse@ynov.com>
2023-11-23 10:47:32 +0100

    for-numbers.php

---
+++ for-numbers.php
@@ -0,0 +1,10 @@
+<?php
+
+for ($i = 2; $i < 100; $i++) {
+    $isPrime = true;
+    for ($j = 2; $j < $i; $j++) {
+        if ($i % $j == 0) $isPrime = false;
+    }
+    if ($isPrime) echo $i . ', ';
+}
+?>
