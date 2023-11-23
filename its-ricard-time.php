b99b20de17e6864bc9ee91d239782afd843cff45
Alexy845 <alexy.daubresse@ynov.com>
2023-11-23 10:36:50 +0100

    its-ricard-time.php

---
+++ its-ricard-time.php
@@ -0,0 +1,13 @@
+<?php
+
+$nbrRicard = 0;
+do {
+    echo "Come on ricard number $nbrRicard\n";
+    $nbrRicard++;
+    if ($nbrRicard == 3) {
+        echo "I'll have to stop soon!\n";
+    }
+    if ($nbrRicard == 5) {
+        echo "I am no longer very fresh ...\n";
+    }
+} while ($nbrRicard <= 7);
