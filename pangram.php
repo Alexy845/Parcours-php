ff91e6484d0c29a265bf551bed22f3613d1a8e83
Alexy845 <alexy.daubresse@ynov.com>
2023-11-23 14:15:07 +0100

    pangram.php

--- pangram.php
+++ pangram.php
@@ -1,6 +1,6 @@
 <?php
 
-function isPangram($string) {
+function isPangram($string) : bool {
     $string = strtolower($string);
     $string = preg_replace('/[^a-z]/', '', $string);
     $string = str_split($string);
