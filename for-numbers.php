ed1382bae8c3b2d75c7d5fef01c9763689b0ae89
Alexy845 <alexy.daubresse@ynov.com>
2023-11-23 11:29:40 +0100

    for-numbers.php

--- for-numbers.php
+++ for-numbers.php
@@ -1,10 +1,11 @@
 <?php
-
+$primes = "";
 for ($i = 2; $i < 100; $i++) {
     $isPrime = true;
     for ($j = 2; $j < $i; $j++) {
-        if ($i % $j == 0) $isPrime = false;
-    }
-    if ($isPrime) echo $i . ', ';
-}
+        if ($i % $j == 0) {
+            $isPrime = false;
+            break;}}
+    if ($isPrime) $primes .= $i . ', ';}
+echo rtrim($primes, ', ');
 ?>
