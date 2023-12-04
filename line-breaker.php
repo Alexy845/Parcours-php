01db36ef37b0aacbc72f43efe1ae3e798836bf8d
Alexy845 <alexy.daubresse@ynov.com>
2023-12-04 09:51:25 +0100

    line breaker

--- line-breaker.php
+++ line-breaker.php
@@ -1,40 +1,44 @@
 <?php
-function breakLines($string, $maxLength) : string {
+function breakLines($string, $maxLength): string {
     $lines = explode("\n", $string);
     $result = '';
 
     foreach ($lines as $line) {
         $words = explode(" ", $line);
         $lineLength = 0;
-
+        $wordCount = 0;
         foreach ($words as $word) {
             $wordLength = strlen($word);
-
             if ($wordLength > $maxLength) {
-                $result .= rtrim($result) !== '' ? "\n" : '';
+
+                $result .= rtrim($result) !== '' ? "\n" : ''; 
                 $result .= $word . "\n";
                 $lineLength = 0;
-            } elseif ($lineLength === 0 || $lineLength + $wordLength <= $maxLength) {
+                $wordCount = 0;
+            } elseif ($lineLength === 0 || ($lineLength + $wordLength + 1 <= $maxLength && $wordCount < 2)) {
                 if ($lineLength > 0) {
                     $result .= ' '; 
                     $lineLength++;
                 }
                 $result .= $word;
                 $lineLength += $wordLength;
+                $wordCount++;
             } else {
                 $result .= "\n" . $word;
                 $lineLength = $wordLength;
+                $wordCount = 1;
             }
         }
-
         if ($result !== '') {
             $result .= "\n"; 
         }
     }
 
-    return rtrim($result);
+    return rtrim($result, "\n");
 }
 
-echo breakLines('Title is long Line with words break', 12);
-echo breakLines("Line with words should break at this spot", 15);
+echo breakLines('Title is long
+Line with words break', 12);
+
+
 
