80ddc5857239b9f28c8e8f09fef8ad8392c4123a
Alexy845 <alexy.daubresse@ynov.com>
2023-12-04 15:49:57 +0100

    recusrsive

--- recursive.php
+++ recursive.php
@@ -5,6 +5,6 @@ function factorial(int $number): int {
     } elseif ($number === 0 || $number === 1) {
         return 1;
     } else {
-        return (int)$number * factorial($number - 1);
+        return int($number * factorial($number - 1));
     }
 }
