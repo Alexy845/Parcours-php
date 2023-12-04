f211f6b2de5d128b2bc6ba4b7e6a8e27bd6b4bda
Alexy845 <alexy.daubresse@ynov.com>
2023-12-04 10:35:17 +0100

    line breaker

--- line-breaker.php
+++ line-breaker.php
@@ -1,23 +1,29 @@
 <?php
-function breakLines($string, $maxLength) : string {
-    $words = explode(" ", $string);
+function breakLines(string $string, int $maxLength): string {
+    $lines = explode("\n", $string);
     $result = '';
-    $lineLength = 0;
 
-    foreach ($words as $word) {
-        $wordLength = strlen($word);
-        
-        if ($lineLength + $wordLength <= $maxLength) {
-            $result .= $word . ' ';
-            $lineLength += $wordLength + 1;
-        } else {
-            $result = rtrim($result);
-            $result .= "\n" . $word . ' ';
-            $lineLength = $wordLength + 1;
+    foreach ($lines as $line) {
+        $words = explode(" ", $line);
+        $lineLength = 0;
+
+        foreach ($words as $word) {
+            $wordLength = strlen($word);
+
+            if ($lineLength + $wordLength <= $maxLength) {
+                $result .= $word . ' ';
+                $lineLength += $wordLength + 1;
+            } else {
+                $result = rtrim($result);
+                $result .= "\n" . $word . ' ';
+                $lineLength = $wordLength + 1;
+            }
         }
+        $result .= "\n";
     }
 
     return rtrim($result);
 }
+
 
 echo breakLines('Title is long Line with words break', 12);
