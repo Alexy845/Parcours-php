49e52283cbb9ac6831fb7ef14e191a102af9d1e4
Alexy845 <alexy.daubresse@ynov.com>
2023-12-04 09:11:58 +0100

    line breaker

--- line-breaker.php
+++ line-breaker.php
@@ -1,37 +1,27 @@
-
 <?php
-function breakLines(string $string, int $length): string {
-    $string = preg_replace('/\s+/', ' ', $string);
-    $lines = explode("\n", $string);
+function breakLines($string, $maxLength) {
+    $words = explode(" ", $string);
     $result = '';
+    $lineLength = 0;
 
-    foreach ($lines as $line) {
-        $words = explode(' ', $line);
-        $currentLine = '';
+    foreach ($words as $word) {
+        $wordLength = strlen($word);
 
-        foreach ($words as $word) {
-            $word = trim($word);
-
-            if (strlen($currentLine) + strlen($word) <= $length) {
-                $currentLine .= $word . ' ';
-            } else {
-                if (!empty($currentLine)) {
-                    $result .= rtrim($currentLine) . "\n";
-                    $currentLine = '';
-                }
-                
-                $currentLine .= $word . ' ';
-            }
-        }
-
-        if (!empty($currentLine)) {
-            $result .= rtrim($currentLine) . "\n";
+        if ($lineLength + $wordLength <= $maxLength) {
+            $result .= $word . ' ';
+            $lineLength += $wordLength + 1;
+        } else {
+            $result = rtrim($result);
+            $result .= "\n" . $word . ' ';
+            $lineLength = $wordLength + 1;
         }
     }
 
     return rtrim($result);
 }
 
-echo breakLines('Line with words', 15); // returns "Line with words"
-echo breakLines("Line with words should break", 15); // returns "Line with words\nshould break"
-echo breakLines("Line with words should break at this spot", 15); // returns "Line with words\nshould break at\nthis spot"
+echo breakLines('Line with words', 15);
+echo "\n";
+echo breakLines("Line with words should break", 15);
+echo "\n";
+echo breakLines("Line with words should break at this spot", 15);
