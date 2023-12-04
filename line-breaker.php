151415e4ac36ca928a24b9f68b43fbbf412c6d4b
Alexy845 <alexy.daubresse@ynov.com>
2023-12-04 09:44:39 +0100

    line breaker

--- line-breaker.php
+++ line-breaker.php
@@ -1,5 +1,5 @@
 <?php
-function breakLines($string, $maxLength) : string {
+function breakLines($string, $maxLength) {
     $lines = explode("\n", $string);
     $result = '';
 
@@ -16,23 +16,25 @@ function breakLines($string, $maxLength) : string {
                 $lineLength = 0;
             } elseif ($lineLength === 0 || $lineLength + $wordLength <= $maxLength) {
                 if ($lineLength > 0) {
-                    $result .= ' ';
+                    $result .= ' '; 
                     $lineLength++;
                 }
                 $result .= $word;
                 $lineLength += $wordLength;
             } else {
-
                 $result .= "\n" . $word;
                 $lineLength = $wordLength;
             }
         }
 
-        $result .= "\n"; 
+        if ($result !== '') {
+            $result .= "\n"; 
+        }
     }
 
     return rtrim($result);
 }
 
+echo breakLines('Title is long Line with words break', 12);
 echo breakLines("Line with words should break at this spot", 15);
 
