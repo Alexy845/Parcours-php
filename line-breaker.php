50ee6ebb97f57e81b8f3085bae46321523cdfaf4
Alexy845 <alexy.daubresse@ynov.com>
2023-12-04 10:32:55 +0100

    line breaker

--- line-breaker.php
+++ line-breaker.php
@@ -1,44 +1,23 @@
 <?php
-function breakLines($string, $maxLength): string {
-    $lines = explode("\n", $string);
+function breakLines($string, $maxLength) : string {
+    $words = explode(" ", $string);
     $result = '';
+    $lineLength = 0;
 
-    foreach ($lines as $line) {
-        $words = explode(" ", $line);
-        $lineLength = 0;
-        $wordCount = 0;
-        foreach ($words as $word) {
-            $wordLength = strlen($word);
-            if ($wordLength > $maxLength) {
-
-                $result .= rtrim($result) !== '' ? "\n" : ''; 
-                $result .= $word . "\n";
-                $lineLength = 0;
-                $wordCount = 0;
-            } elseif ($lineLength === 0 || ($lineLength + $wordLength + 1 <= $maxLength && $wordCount < 2)) {
-                if ($lineLength > 0) {
-                    $result .= ' '; 
-                    $lineLength++;
-                }
-                $result .= $word;
-                $lineLength += $wordLength;
-                $wordCount++;
-            } else {
-                $result .= "\n" . $word;
-                $lineLength = $wordLength;
-                $wordCount = 1;
-            }
-        }
-        if ($result !== '') {
-            $result .= "\n"; 
+    foreach ($words as $word) {
+        $wordLength = strlen($word);
+        
+        if ($lineLength + $wordLength <= $maxLength) {
+            $result .= $word . ' ';
+            $lineLength += $wordLength + 1;
+        } else {
+            $result = rtrim($result);
+            $result .= "\n" . $word . ' ';
+            $lineLength = $wordLength + 1;
         }
     }
 
-    return rtrim($result, "\n");
+    return rtrim($result);
 }
 
-echo breakLines('Title is long
-Line with words break', 12);
-
-
-
+echo breakLines('Title is long Line with words break', 12);
