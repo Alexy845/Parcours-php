1345b16f475048cf9a78676aac5fe0676072c219
Alexy845 <alexy.daubresse@ynov.com>
2023-12-04 14:10:07 +0100

    recusrsive

--- recursive.php
+++ recursive.php
@@ -1,12 +1,12 @@
 <?php
-function factorial($number) : int {
+function factorial($number): int {
     if ($number < 0) {
-        return null;
+        return 0;
     }
     
     if ($number == 0 || $number == 1) {
         return 1;
     }
 
-    return (int)$number * int(factorial($number - 1));
+    return gmp_intval(gmp_fact($number));
 }
