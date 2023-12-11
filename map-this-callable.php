eb0051f32c2349477727d502295d4f7462dc228e
Alexy845 <alexy.daubresse@ynov.com>
2023-12-11 11:01:15 +0100

    map-this-callable.php

--- map-this-callable.php
+++ map-this-callable.php
@@ -1,17 +1,16 @@
 <?php
-function myArrayMap(callable $callback, array $array) {
+function myArrayMap(callable $callback, array $array): array{
     $result = [];
-
-    foreach ($array as $value) {
-        $result[] = $callback($value);
+    foreach ($array as $key => $value) {
+        $result[$key] = call_user_func($callback, $value);
     }
-
     return $result;
 }
 
-$array = [1, 2, 3, 4];
-$mapped = myArrayMap(function ($a) {
-    return $a * 2;
+$array = [1, 2, 3, 4, 5];
+
+$multiplied = myArrayMap(function ($value) {
+    return $value * 2;
 }, $array);
 
-print_r($mapped); // [2, 4, 6, 8]
+echo implode(', ', $multiplied); // 2, 4, 6, 8, 10
