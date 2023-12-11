83df58a97819d0bc9cd5693d07c89f08b56a79b4
Alexy845 <alexy.daubresse@ynov.com>
2023-12-11 09:37:50 +0100

    manage-robot.php

---
+++ manage-robot-movements.php
@@ -0,0 +1,36 @@
+<?php
+function manageMovements($movements): array {
+    $instructions = [];
+    $previousMovement = '';
+
+    for ($i = 0; $i < strlen($movements); $i++) {
+        $movement = $movements[$i];
+
+        if (in_array($movement, ['R', 'L', 'F', 'B'])) {
+            if ($movement === $previousMovement) {
+                $instructions[] = 'WARNING: ' . getMovementName($movement) . ' AGAIN';
+            } else {
+                $instructions[] = getMovementName($movement);
+            }
+            $previousMovement = $movement;
+        }
+    }
+
+    return $instructions;
+}
+
+function getMovementName($movement) {
+    switch ($movement) {
+        case 'R':
+            return 'RIGHT';
+        case 'L':
+            return 'LEFT';
+        case 'F':
+            return 'FRONT';
+        case 'B':
+            return 'BACKWARDS';
+    }
+}
+
+echo implode(', ', manageMovements('RFFRF')); // RIGHT, FRONT, RIGHT, FRONT
+echo implode(', ', manageMovements('RFFRFLL')); // RIGHT, FRONT, RIGHT, FRONT, LEFT, LEFT
