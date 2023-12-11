114d0b4e3b60b6d4abc4f931eea6515443ca3822
Alexy845 <alexy.daubresse@ynov.com>
2023-12-11 09:50:09 +0100

    check-the-robot.php

---
+++ check-the-robot-circuits.php
@@ -0,0 +1,28 @@
+<?php
+function checkCircuits($number) {
+    $bugLocations = [];
+
+    if ($number % 2 === 0) {
+        $bugLocations[] = 'Left arm';
+    }
+    if ($number % 3 === 0) {
+        $bugLocations[] = 'Right arm';
+    }
+    if ($number % 5 === 0) {
+        $bugLocations[] = 'Motherboard';
+    }
+    if ($number % 7 === 0) {
+        $bugLocations[] = 'Processor';
+    }
+    if ($number % 11 === 0) {
+        $bugLocations[] = 'Zip Defluxer';
+    }
+    if ($number % 13 === 0) {
+        $bugLocations[] = 'Motor';
+    }
+
+    return $bugLocations;
+}
+
+print_r(checkCircuits(9)); // Output: Array ( [0] => Right arm )
+print_r(checkCircuits(12)); // Output: Array ( [0] => Left arm [1] => Right arm )
