c3a8ade8b0bb713291ec70b5cdac91fbf5b721db
Alexy845 <alexy.daubresse@ynov.com>
2023-12-11 10:22:11 +0100

    dna.php

--- dna-difference.php
+++ dna-difference.php
@@ -1,13 +1,9 @@
 <?php
 function dnaDiff($strand1, $strand2): int|bool {
-    // Vérifier si les longueurs des chaînes sont différentes
     if (strlen($strand1) !== strlen($strand2)) {
         return false;
     }
-
     $differences = 0;
-
-    // Calculer le nombre de différences entre les chaînes d'ADN
     for ($i = 0; $i < strlen($strand1); $i++) {
         if ($strand1[$i] !== $strand2[$i]) {
             $differences++;
@@ -17,5 +13,4 @@ function dnaDiff($strand1, $strand2): int|bool {
     return $differences;
 }
 
-// Test cases
 var_dump(dnaDiff('GAGCCTACTAACGGGAT', 'CATCGTAATGACGGCCTAZA')); //  bool(false)
