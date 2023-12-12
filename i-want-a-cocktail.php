e96d45c4b1e2f67465f3db6f783be0ddde1c6c5e
Alexy845 <alexy.daubresse@ynov.com>
2023-12-12 09:50:39 +0100

    cocktail.php

--- i-want-a-cocktail.php
+++ i-want-a-cocktail.php
@@ -3,11 +3,11 @@ class Mojito {
     public float $alcoholRate = 0.15;
     public array $ingredients = ['rum', 'lime', 'sparkling water', 'mint', 'sugar'];
     public int $price = 8;
-    
+
     public function __construct(float $alcoholRate = 0.15, array $ingredients = [], int $price = 8) {
         $this->alcoholRate = $alcoholRate;
-        $this->ingredients = $ingredients;
+        $this->ingredients = empty($ingredients) ? $this->ingredients : $ingredients;
         $this->price = $price;
     }
-    
+
 }
